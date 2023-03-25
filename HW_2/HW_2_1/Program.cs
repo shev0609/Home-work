0437f3c74ce499b23210e9ccaac76202094e4889
shev0609 <m.lolita653@gmail.com>
2023-03-25 13:57:02 +0500

    HW_2_1

--- HW_2/HW_2_1/Program.cs
+++ HW_2/HW_2_1/Program.cs
@@ -2,8 +2,8 @@
 
 string NumSec(int num)
 {
-if ((-1000 < num && num < -100)|| (num < 1000 && num > 100))
-return $"{num / 10 % 10}";
-return "The number is not three-digit!";
+    if ((-1000 < num && num < -100) || (num < 1000 && num > 100))
+        return $"{num / 10 % 10}";
+    return "The number is not three-digit!";
 }
 Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
---
+++ HW_2/HW_2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// 2. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+string SecondNum(int num)
+{
+if ((-1000 < num && num < -100)|| (num < 1000 && num > 100))
+return $"{num / 10 % 10}";
+return "The number is not three-digit!";
+}
+Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
