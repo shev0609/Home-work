0ea0b0cc912207022bd3f9bb72ae5135490890b6
shev0609 <m.lolita653@gmail.com>
2023-03-25 15:26:20 +0500

    HW_2_2

--- HW_2/HW_2_2/Program.cs
+++ HW_2/HW_2_2/Program.cs
@@ -2,8 +2,8 @@
 
 string SecondNum(int num)
 {
-if ((-1000 < num && num < -100)|| (num < 1000 && num > 100))
-return $"{num / 10 % 10}";
-return "The number is not three-digit!";
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+        return $"{num} -> {num / 100 * 10 + num % 10}";
+    return "The number is not three-digit!";
 }
-Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
+Console.WriteLine(SecondNum(int.Parse(Console.ReadLine())));
