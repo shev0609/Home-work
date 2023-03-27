90ad7c251a0f641396b932364f061376c22dc824
shev0609 <m.lolita653@gmail.com>
2023-03-27 21:11:52 +0500

    hw_6_1

---
+++ HW_6/HW_6_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+
+int CountNum()
+{
+    int count = 0;
+    string word;
+
+    while (true)
+    {
+        Console.Write("Output any number: ");
+        word = Console.ReadLine()!;
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count += 1;        
+    }
+}
+int count = CountNum();
+Console.WriteLine($"Numbers greater than zero: {count}");
