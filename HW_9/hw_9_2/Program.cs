88e62f1df6d798815f760362bae31f0b3df108c1
shev0609 <m.lolita653@gmail.com>
2023-03-29 22:36:21 +0500

    hw_9_2

---
+++ HW_9/hw_9_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.Clear();
+Console.Write("Введите числа M и N: ");
+
+int EvenSum(int M, int N)
+{
+    while (M < 1) M++;
+    if (M > N) return 0;
+    return EvenSum(M+1, N) + M;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(EvenSum(num, num2));
